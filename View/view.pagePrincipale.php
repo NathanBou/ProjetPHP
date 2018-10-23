a491e7f57e667650add828f9a83a7546b3465561
BOUVIER Nathan <bouviern@hostname.iut2.upmf-grenoble.fr>
2018-10-23 08:10:29 +0200

    SAlut

---
+++ View/view.pagePrincipale.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Music-all</title>
+  </head>
+  <body>
+      <header>
+        <div class="MainMenu">
+          <div class="col1">
+            <a href=""><img src="models/logo.jpg" alt="Logo du site" width="238" height="153"></a>
+          </div>
+          <div class="col2">
+            <div class="ligne1">
+              <span class="sepHome">|</span>
+              <a class="lienHome" href=""><span>Accueil</span></a>
+              <span class="sepHome">|</span>
+              <a class="lienConnexion" href="view.pageConnexion.html"><span>Compte</span></a>
+              <span class="sepHome">|</span>
+              <a href="PBShoppingCart.asp?PBMInit=1">
+                <span class="prixPanier"><p>0,00 <?= $prixPanier ?> € </p></span>
+              </a>
+            </div>
+            <div class="ligne2">
+              <h1>Music-all</h1>
+            </div>
+          </div>
+        </div>
+
+      </header>
+
+  </body>
+</html>
