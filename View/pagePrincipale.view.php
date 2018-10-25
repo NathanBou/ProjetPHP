96ec44c2663e14844b8a6dd3f64e879a5ab7d4e0
BOUVIER Nathan <bouviern@hostname.iut2.upmf-grenoble.fr>
2018-10-25 09:02:08 +0200

    a

--- View/pagePrincipale.view.php
+++ View/pagePrincipale.view.php
@@ -34,43 +34,42 @@
                 </div>
               </div>
           </div>
-            <div class="MenuDeroulant">
-              <nav>
-                <ul>
-                  <li class="menu-instruments"><a href="">Instruments</a>
-                      <ul class="sousmenu">
-                        <li><a href="#">Instruments à Cordes</a></li>
-                        <li><a href="#">Instruments à Vents</a></li>
-                        <li><a href="#">Percussions</a></li>
-                      </ul>
-                  </li>
-                  <li class="menu-accessoires"><a href="#">Accessoires</a>
-                      <ul class="sousmenu">
-                        <li><a href="#">Mediator</a></li>
-                        <li><a href="#">Bec</a></li>
-                        <li><a href="#">Cordes</a></li>
-                        <li><a href="#">Pupitre</a></li>
-                        <li><a href="#">Diapason</a></li>
-                        <li><a href="#">Accordeur</a></li>
-                      </ul>
-                  </li>
-                  <li class="menu-partitions"><a href="#">Partitions</a>
-                    <ul class="sousmenu">
-                      <li><a href="#">Bach</a></li>
-                      <li><a href="#">Mozart</a></li>
-                      <li><a href="#">Chopin</a></li>
-                      <li><a href="#">Vivaldi</a></li>
-                      <li><a href="#">Ravel</a></li>
-                      <li><a href="#">Beethoven</a></li>
-                      <li><a href="#">Schubert</a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </nav>
-            </div>
           </div>
       </div>
+      <div class="MenuDeroulant">
+        <nav>
+          <ul>
+            <li class="menu-instruments"><a href="">Instruments</a>
+                <ul class="sousmenu">
+                  <li><a href="#">Instruments à Cordes</a></li>
+                  <li><a href="#">Instruments à Vents</a></li>
+                  <li><a href="#">Percussions</a></li>
+                </ul>
+            </li>
+            <li class="menu-accessoires"><a href="#">Accessoires</a>
+                <ul class="sousmenu">
+                  <li><a href="#">Mediator</a></li>
+                  <li><a href="#">Bec</a></li>
+                  <li><a href="#">Cordes</a></li>
+                  <li><a href="#">Pupitre</a></li>
+                  <li><a href="#">Diapason</a></li>
+                  <li><a href="#">Accordeur</a></li>
+                </ul>
+            </li>
+            <li class="menu-partitions"><a href="#">Partitions</a>
+              <ul class="sousmenu">
+                <li><a href="#">Bach</a></li>
+                <li><a href="#">Mozart</a></li>
+                <li><a href="#">Chopin</a></li>
+                <li><a href="#">Vivaldi</a></li>
+                <li><a href="#">Ravel</a></li>
+                <li><a href="#">Beethoven</a></li>
+                <li><a href="#">Schubert</a></li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
       </header>
-
   </body>
 </html>
