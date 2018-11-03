a4a121862f6029fc85c49ba4d5929ddb4c9faa69
BOUVIER Nathan <bouviern@hostname.iut2.upmf-grenoble.fr>
2018-11-03 21:51:42 +0100

    a

--- View/pagePrincipale.view.php
+++ View/pagePrincipale.view.php
@@ -7,11 +7,19 @@
   </head>
   <body>
       <header>
+        <?php
+          global $parametre;
+          if($connect){
+            $parametre="connect=".$user;
+          }else{
+            $parametre="connect=";
+          }
+          ?>
         <div class="MainMenu">
           <div class="MenuTot">
             <div class="MenuEste">
               <div class="col1">
-                <a href=""><img src="../View/models/logo.jpg" alt="Logo du site" width="238" height="153"></a>
+                <a href="main.controler.php?<?php echo $parametre; ?>"><img src="../View/models/logo.jpg" alt="Logo du site" width="238" height="153"></a>
               </div>
               <div class="col2">
                 <div class="ligne1">
@@ -22,10 +30,15 @@
                   <span class="sepHome">|</span>
                   <div class="Panier">
                     <a href="../View/panier.view.php">
-                      <img src="../View/models/panier.png" alt="panier" width="23" height="23">
+                      <img src="../View/models/panier.png" alt="panier" width="22" height="22">
                       <span class="prixPanier"><?=$prixPanier?>€</span>
                     </a>
                   </div>
+                  <span class="sepHome">|</span>
+                  <?php if($connect){ ?>
+                    <a href="main.controler.php?connect="> <img src="../View/models/logout.png" alt="logout" width="23" height="23"> </a>
+                    <span class="sepHome">|</span>
+                  <?php } ?>
                 </div>
                 <div class="ligne2">
                   <div class="slogan">
@@ -38,14 +51,6 @@
           </div>
       </div>
       <div class="MenuDeroulant">
-        <?php
-          global $parametre;
-          if($connect){
-            $parametre="&connect=".$user;
-          }else{
-            $parametre="&connect=";
-          }
-          ?>
         <nav>
           <ul>
             <li class="menu-instruments"><a href="">Instruments</a>
@@ -53,50 +58,50 @@
                   <li><a href="#">Instruments à Cordes</a>
                     <ul class="sousmenu2">
 
-                      <li><a href="../controler/main.controler.php?categorie=Guitare<?php echo $parametre; ?>">Guitare</a></li>
-                      <li><a href="../controler/main.controler.php?categorie=Piano<?php echo $parametre; ?>">Piano</a></li>
-                      <li><a href="../controler/main.controler.php?categorie=Violon<?php echo $parametre; ?>">Violon</a></li>
+                      <li><a href="../controler/main.controler.php?categorie=Guitare&<?php echo $parametre; ?>">Guitare</a></li>
+                      <li><a href="../controler/main.controler.php?categorie=Piano&<?php echo $parametre; ?>">Piano</a></li>
+                      <li><a href="../controler/main.controler.php?categorie=Violon&<?php echo $parametre; ?>">Violon</a></li>
                     </ul>
                   </li>
                   <li><a href="#">Instruments à Vents</a>
                     <ul class="sousmenu2">
                       <li><a href="#">Bois</a>
                         <ul class="sousmenu3">
-                          <li><a href="../controler/main.controler.php?categorie=Clarinette<?php echo $parametre; ?>">Clarinette</a></li>
-                          <li><a href="../controler/main.controler.php?categorie=Saxophone<?php echo $parametre; ?>">Saxophone</a></li>
+                          <li><a href="../controler/main.controler.php?categorie=Clarinette&<?php echo $parametre; ?>">Clarinette</a></li>
+                          <li><a href="../controler/main.controler.php?categorie=Saxophone&<?php echo $parametre; ?>">Saxophone</a></li>
                         </ul>
                       </li>
                       <li><a href="#">Cuivres</a>
                         <ul class="sousmenu3">
-                          <li><a href="../controler/main.controler.php?categorie=Cor<?php echo $parametre; ?>">Cor</a></li>
-                          <li><a href="../controler/main.controler.php?categorie=Trompette<?php echo $parametre; ?>">Trompette</a></li>
-                          <li><a href="../controler/main.controler.php?categorie=Trombone<?php echo $parametre; ?>">Trombone</a></li>
+                          <li><a href="../controler/main.controler.php?categorie=Cor&<?php echo $parametre; ?>">Cor</a></li>
+                          <li><a href="../controler/main.controler.php?categorie=Trompette&<?php echo $parametre; ?>">Trompette</a></li>
+                          <li><a href="../controler/main.controler.php?categorie=Trombone&<?php echo $parametre; ?>">Trombone</a></li>
                         </ul>
                       </li>
                     </ul>
                   </li>
-                  <li><a href="../controler/main.controler.php?categorie=Percussions<?php echo $parametre; ?>">Percussions</a></li>
+                  <li><a href="../controler/main.controler.php?categorie=Percussions&<?php echo $parametre; ?>">Percussions</a></li>
                 </ul>
             </li>
             <li class="menu-accessoires"><a href="#">Accessoires</a>
                 <ul class="sousmenu">
-                  <li><a href="../controler/main.controler.php?categorie=Mediator<?php echo $parametre; ?>">Mediator</a></li>
-                  <li><a href="../controler/main.controler.php?categorie=Bec<?php echo $parametre; ?>">Bec</a></li>
-                  <li><a href="../controler/main.controler.php?categorie=Cordes<?php echo $parametre; ?>">Cordes</a></li>
-                  <li><a href="../controler/main.controler.php?categorie=Pupitre<?php echo $parametre; ?>">Pupitre</a></li>
-                  <li><a href="../controler/main.controler.php?categorie=Diapason<?php echo $parametre; ?>">Diapason</a></li>
-                  <li><a href="../controler/main.controler.php?categorie=Accordeur<?php echo $parametre; ?>">Accordeur</a></li>
+                  <li><a href="../controler/main.controler.php?categorie=Mediator&<?php echo $parametre; ?>">Mediator</a></li>
+                  <li><a href="../controler/main.controler.php?categorie=Bec&<?php echo $parametre; ?>">Bec</a></li>
+                  <li><a href="../controler/main.controler.php?categorie=Cordes&<?php echo $parametre; ?>">Cordes</a></li>
+                  <li><a href="../controler/main.controler.php?categorie=Pupitre&<?php echo $parametre; ?>">Pupitre</a></li>
+                  <li><a href="../controler/main.controler.php?categorie=Diapason&<?php echo $parametre; ?>">Diapason</a></li>
+                  <li><a href="../controler/main.controler.php?categorie=Accordeur&<?php echo $parametre; ?>">Accordeur</a></li>
                 </ul>
             </li>
             <li class="menu-partitions"><a href="#">Partitions</a>
               <ul class="sousmenu">
-                <li><a href="../controler/main.controler.php?categorie=Bach<?php echo $parametre; ?>">Bach</a></li>
-                <li><a href="../controler/main.controler.php?categorie=Mozart<?php echo $parametre; ?>">Mozart</a></li>
-                <li><a href="../controler/main.controler.php?categorie=Chopin<?php echo $parametre; ?>">Chopin</a></li>
-                <li><a href="../controler/main.controler.php?categorie=Vivaldi<?php echo $parametre; ?>">Vivaldi</a></li>
-                <li><a href="../controler/main.controler.php?categorie=Ravel<?php echo $parametre; ?>">Ravel</a></li>
-                <li><a href="../controler/main.controler.php?categorie=Beethoven<?php echo $parametre; ?>">Beethoven</a></li>
-                <li><a href="../controler/main.controler.php?categorie=Schubert<?php echo $parametre; ?>">Schubert</a></li>
+                <li><a href="../controler/main.controler.php?categorie=Bach&<?php echo $parametre; ?>">Bach</a></li>
+                <li><a href="../controler/main.controler.php?categorie=Mozart&<?php echo $parametre; ?>">Mozart</a></li>
+                <li><a href="../controler/main.controler.php?categorie=Chopin&<?php echo $parametre; ?>">Chopin</a></li>
+                <li><a href="../controler/main.controler.php?categorie=Vivaldi&<?php echo $parametre; ?>">Vivaldi</a></li>
+                <li><a href="../controler/main.controler.php?categorie=Ravel&<?php echo $parametre; ?>">Ravel</a></li>
+                <li><a href="../controler/main.controler.php?categorie=Beethoven&<?php echo $parametre; ?>">Beethoven</a></li>
+                <li><a href="../controler/main.controler.php?categorie=Schubert&<?php echo $parametre; ?>">Schubert</a></li>
               </ul>
             </li>
           </ul>
@@ -113,8 +118,11 @@
           <?= $value->__get("libelle") ?> <br>
           Informations complémentaires : <?= $value->__get("infoscompl") ?> <br>
           Prix : <?= $value->__get("prix") ?><br>
+          Ref : <?= $value->__get("ref") ?> <br>
           <?php if($connect){?>
             <a href="#"> <img src="../View/models/panier.png" alt="AjoutezAuPanier" width="23" height="23">Ajoutez au panier</a>
+          <?php }else{?>
+            <a href="../View/pageConnexion.view.php">Connectez vous !</a>
           <?php } ?>
           </div>
           <?php } ?>
