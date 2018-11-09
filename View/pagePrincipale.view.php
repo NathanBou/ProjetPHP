7d78af37fc603d5e9ac336200e943f728c9f2038
BOUVIER Nathan <bouviern@hostname.iut2.upmf-grenoble.fr>
2018-11-09 16:12:12 +0100

    a

--- View/pagePrincipale.view.php
+++ View/pagePrincipale.view.php
@@ -28,7 +28,7 @@
               <div class="col2">
                 <div class="ligne1">
                   <span class="sepHome">|</span>
-                  <a class="lienHome" href=""><span>Accueil</span></a>
+                  <a class="lienHome" href="main.controler.php?<?php echo $parametre; ?>"><span>Accueil</span></a>
                   <span class="sepHome">|</span>
                   <?php if($connect){echo $user;}else{echo "<a class=\"lienConnexion\" href=\"../View/pageConnexion.view.php\">Compte</a>";}?>
                   <span class="sepHome">|</span>
