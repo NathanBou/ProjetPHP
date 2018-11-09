5bbb77b45b2eab132bf55d7d9aac2fd3ac29e11a
BOUVIER Nathan <bouviern@hostname.iut2.upmf-grenoble.fr>
2018-11-09 16:59:10 +0100

    a

--- View/pagePrincipale.view.php
+++ View/pagePrincipale.view.php
@@ -184,7 +184,7 @@
           <a href="panier.controler.php?<?php echo $parametre; ?>&retirer=all"><input type="submit" name="vider" value="Videz le panier"> </a>
             </div>
         <?php }else{ ?>
-          <img src="../View/models/accueil.jpg" alt="photo accueil" width="100%" height="75%"">
+          <img src="../View/models/accueil.jpg" alt="photo accueil" width="100%" height="75%">
         <?php } ?>
       </div>
   </body>
